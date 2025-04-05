fb0c8b2488e6974200b081b8d8f59e83cc23f9e0
Dhruv Hegde <ddvhegde100@gmail.com>
2025-04-05 10:33:54 -0400

    firebase stuff

---
+++ assets/js/auth.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCueN8ljuq855hxvYBgH-Odu1nc_1DBvj0",
+    authDomain: "recsend-9d190.firebaseapp.com",
+    projectId: "recsend-9d190",
+    storageBucket: "recsend-9d190.firebasestorage.app",
+    messagingSenderId: "155357247380",
+    appId: "1:155357247380:web:3b5aca103e7f53a9328181",
+    measurementId: "G-Q5BEN64PTX"
+  };
+
+firebase.initializeApp(firebaseConfig);
